e01a7e148e4864eed5ef4eba4bf9d8429adb4aea
root <thongal@pluribusnetworks.com>
2021-09-01 21:14:23 -0700

    Initial demo commit.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pn-demo1
+# Copyright Pluribus Networks 2021
---
+++ runtime-evn.sh
@@ -0,0 +1,8 @@
+CNI_COMMAND= ADD, DELETE, CHECK, VERSION
+#TBD Consider automating this to be unique per invocation
+CNI_CONTAINERID=pn1
+CNI_NETNS=/proc/<pid>/ns/net
+CNI_IFNAME=eth0
+
+#location where plugin is  stored.
+CNI_PATH=/opt/cni/bin	
